de499dd864cabfd3db1c961993bc4d72f600f1d9
psimao <psimao@ciandt.com>
2017-07-26 20:46:26 -0300

    Added Binary Gap, Cyclic Rotation, Frog Jmp, Perm Missing Elem and Tape Equilibrium solutions + README.md

---
+++ 02_cyclic_rotation/Solution.java
@@ -0,0 +1,48 @@
+/*
+* A zero-indexed array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is also moved to the first place.
+* 
+* For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]. The goal is to rotate array A K times; that is, each element of A will be shifted to the right by K indexes.
+* 
+* Write a function:
+* 
+* class Solution { public int[] solution(int[] A, int K); }
+* 
+* that, given a zero-indexed array A consisting of N integers and an integer K, returns the array A rotated K times.
+* 
+* For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].
+* 
+* Assume that:
+* 
+* N and K are integers within the range [0..100];
+* each element of array A is an integer within the range [−1,000..1,000].
+* In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+*/
+
+class Solution {
+
+    public int[] solution(int[] array, int shift) {
+        if (shift == 0 || array.length <= 1) {
+            return array;
+        }
+
+        int[] shiftedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        
+        for (int i = 0; i < array.length; i++) {
+            shiftedArray[calculateShiftedIndex(i, maxIndex, shift)] = array[i];
+        }
+
+        return shiftedArray;
+    }
+
+    public int calculateShiftedIndex(int currentIndex, int maxIndex, int shift) {
+        int shiftedIndex;
+        if ((shiftedIndex = currentIndex + shift) > maxIndex) {
+            while (shiftedIndex > maxIndex) {
+                shiftedIndex = shiftedIndex - maxIndex - 1;
+            }
+        }
+
+        return shiftedIndex;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # codility-demo
-Codility demos made for training 👾
+Some of [codility lessons](https://codility.com/programmers/lessons) solutions 🖖
+
+## Solution List
+* [01. Binary Gap](https://github.com/pedroartsimao/codility-demo/blob/master/01_binary_gap/Solution.java)
+* [02. Cyclic Rotation](https://github.com/pedroartsimao/codility-demo/blob/master/02_cyclic_rotation/Solution.java)
+* [03. Frog Jmp](https://github.com/pedroartsimao/codility-demo/blob/master/03_frog_jmp/Solution.java)
+* [03. Perm Missing Elem](https://github.com/pedroartsimao/codility-demo/blob/master/03_perm_missing_elem/Solution.java)
+* [03. Tape Equilibrium](https://github.com/pedroartsimao/codility-demo/blob/master/03_tape_equilibrium/Solution.java)
